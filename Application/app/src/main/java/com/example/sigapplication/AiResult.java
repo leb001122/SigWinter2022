0791d9cba10bbfb872db94349e57f9a90d768e9f
leb001122 <leb001122@icloud.com>
2022-02-07 03:17:27 +0900

    pager

--- Application/app/src/main/java/com/example/sigapplication/AiResult.java
+++ Application/app/src/main/java/com/example/sigapplication/AiResult.java
@@ -8,26 +8,12 @@ public class AiResult extends AppCompatActivity {
     private final int FRAGMENT_1 = 1;
     private final int FRAGMENT_2 = 2;
 
-    ResultFragment fragment;
-    ResultFragment2 fragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ai_result);
 
-        fragment = (ResultFragment) getSupportFragmentManager().findFragmentById(R.id.result1);
-        fragment2 = new ResultFragment2();
 
-    }
-
-    public void onFragmentChanged(int index) {
-        if (index == 0) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-        }
-        else if (index == 1) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
-
-        }
     }
 }
