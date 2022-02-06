2199a445cb2d20c69aaad37ac1e8ee12b84c4892
leb001122 <leb001122@icloud.com>
2022-02-07 03:57:04 +0900

    pager

--- Application/app/src/main/java/com/example/sigapplication/AiResult.java
+++ Application/app/src/main/java/com/example/sigapplication/AiResult.java
@@ -1,19 +1,63 @@
 package com.example.sigapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class AiResult extends AppCompatActivity {
 
-    private final int FRAGMENT_1 = 1;
-    private final int FRAGMENT_2 = 2;
-
+    private static final int NUM_PAGES = 3;
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ai_result);
 
+        pager = findViewById(R.id.viewpager);
+        pager.setOffscreenPageLimit(NUM_PAGES);
 
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+
+        Fragment1 fragment1 = new Fragment1();
+        adapter.addItem(fragment1);
+
+        Fragment2 fragment2 = new Fragment2();
+        adapter.addItem(fragment2);
+
+        Fragment3 fragment3 = new Fragment3();
+        adapter.addItem(fragment3);
+
+        pager.setAdapter(adapter);
+    }
+
+    class PagerAdapter extends FragmentStatePagerAdapter {
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item) {
+            items.add(item);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
     }
 }
