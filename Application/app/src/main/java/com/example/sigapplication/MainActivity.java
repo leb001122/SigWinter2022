ce028a47870a4e27429d5f8ad30e7c60a3099794
leb001122 <leb001122@icloud.com>
2022-02-07 17:53:47 +0900

    .

--- Application/app/src/main/java/com/example/sigapplication/MainActivity.java
+++ Application/app/src/main/java/com/example/sigapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.View;
@@ -17,12 +18,24 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private ArrayList<MyQuestion> questions;
+    private MyQuestionAdapter adapter;
+    Button btnAdd;
+    DBHelper helper;
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnAdd = (Button) findViewById(R.id.btn_question_add);
+        btnAdd = (Button) findViewById(R.id.btn_question_add);
+        listView = (ListView) findViewById(R.id.listview);
+        questions = new ArrayList<MyQuestion>();
+        helper = new DBHelper(MainActivity.this, "sigdb.db", null, 1);
+        db = helper.getWritableDatabase();
+        helper.onCreate(db);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,19 +44,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        //어댑터 초기화부분. questions와 어댑터를 연결해준다.
+        adapter = new MyQuestionAdapter(getApplicationContext(), questions);
+        listView.setAdapter(adapter);
+
+        /*
+
         final ArrayList<String> midList = new ArrayList<>();
         ListView list = (ListView) findViewById(R.id.listview);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, midList);
         list.setAdapter(adapter);
 
-/*        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+*//*        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int index, long id) {
                 Toast.makeText(getApplicationContext(), midList[index], Toast.LENGTH_SHORT).show();
             }
         });
-        */
+        *//*
 
         // item 제거
         list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -62,6 +82,6 @@ public class MainActivity extends AppCompatActivity {
                 midList.add((midList.size()+1) + "번째");
                 adapter.notifyDataSetChanged();
             }
-        });
+        });*/
     }
 }
